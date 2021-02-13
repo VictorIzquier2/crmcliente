670dd97dd3a1758f5fbc7c8a5426eb92d310fe25
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-02-13 09:24:27 +0100

    alertas de edicion

--- pages/editarproducto/[pid].js
+++ pages/editarproducto/[pid].js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import {useRouter} from 'next/router';
-import {useQuery, gql} from '@apollo/client';
+import {useQuery, useMutation, gql} from '@apollo/client';
 import {Formik} from 'formik';
 import * as Yup from 'yup';
+import Swal from 'sweetalert2';
 
 const OBTENER_PRODUCTO = gql`
   query obtenerProducto($id: ID!) {
@@ -15,11 +16,23 @@ const OBTENER_PRODUCTO = gql`
   }
 `;
 
+const ACTUALIZAR_PRODUCTO = gql`
+  mutation actualizarProducto($id: ID!, $input: ProductoInput){
+
+    actualizarProducto(id: $id, input: $input){
+      id
+      nombre
+      existencia
+      precio
+    }
+  }
+`;
+
 const EditarProducto = () => {
   const router = useRouter();
 
   const {query: {id}} = router;
-  console.log(id);
+  //console.log(id);
 
   const {data, loading, error} = useQuery(OBTENER_PRODUCTO, {
     variables: {
@@ -27,97 +40,159 @@ const EditarProducto = () => {
     }
   });
 
-  console.log(data);
-  console.log(loading);
-  console.log(error);
+  // Mutation 
+  const [actualizarProducto] = useMutation(ACTUALIZAR_PRODUCTO);
+
+  // Schema de validacion 
+  const schemaValidation = Yup.object({
+    nombre: Yup
+              .string()
+              .required('El nombre del producto es obligatorio'),
+    existencia: Yup
+                  .number()
+                  .required('Agrega la cantidad disponible')
+                  .positive('No se acepta 0 o números negativos')
+                  .integer('El número de existencia debe ser un entero'),
+    precio: Yup
+              .number()
+              .required('El precio es obligatorio')
+              .positive('No se aceptan números negativos')
+  });
+
 
   if(loading) return 'Cargando...';
 
-  const {obtenerProducto} = data;
+  if(!data) return 'Acción no permitida';
 
+  const actualizarInfoProducto = async valores => {
+    //console.log(valores);
+    const {nombre, existencia, precio} = valores;
+
+    try{
+      const {data} = await actualizarProducto({
+        variables: {
+          id,
+          input: {
+            nombre,
+            existencia,
+            precio
+          }
+        }
+      });
+      //console.log(data);
+
+      // Redirigir hacia productos 
+      router.push('/productos')
+
+      // Mostrar una alerta
+      Swal.fire(
+        'Correcto',
+        'El producto se actualizó correctamente',
+        'success'
+      )
+
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  const {obtenerProducto} = data;
+  
   return ( 
     <Layout>
       <h1 className='text-2xl text-gray-800 font-light'>Desde Editar Producto</h1>
       <div className="flex justify-center mt-5">
         <div className='w-full max-w-lg'>
-          <Formik>
-            <form
-              className='bg-white shadow-md px-8 pt-6 pb-8 mb-4'
-              //onSubmit={formik.handleSubmit}
-            >
-              <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='nombre'>
-                  Nombre
-                </label>
-                <input
-                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  id='nombre'
-                  type='text'
-                  placeholder='Nombre Producto'
-                  //onChange={formik.handleChange}
-                  //onBlur={formik.handleBlur}
-                  //value={formik.values.nombre}
-                />
-              </div>
+          <Formik
+            enableReinitialize
+            initialValues={obtenerProducto}
+            validationSchema={schemaValidation}
+            onSubmit={valores => {
+              actualizarInfoProducto(valores);
+            }}
+          >
+            {props => {
+              return(
 
-              {/*formik.touched.nombre && formik.errors.nombre ? (
-                <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4'>
-                  <p className='font-bold'>Error</p>
-                  <p>{formik.errors.nombre}</p>
-                </div>
-              ) : null*/}
+                <form
+                  className='bg-white shadow-md px-8 pt-6 pb-8 mb-4'
+                  onSubmit={props.handleSubmit}
+                  >
+                  <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='nombre'>
+                    Nombre
+                    </label>
+                    <input
+                    className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                    id='nombre'
+                    type='text'
+                    placeholder='Nombre Producto'
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.nombre}
+                    />
+                  </div>
+                  
+                  {props.touched.nombre && props.errors.nombre ? (
+                    <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4'>
+                    <p className='font-bold'>Error</p>
+                    <p>{props.errors.nombre}</p>
+                    </div>
+                  ) : null}
 
-              <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='existencia'>
-                  Existencia
-                </label>
-                <input
-                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  id='existencia'
-                  type='number'
-                  placeholder='Existencia Producto'
-                  //onChange={formik.handleChange}
-                  //onBlur={formik.handleBlur}
-                  //value={formik.values.existencia}
-                />
-              </div>
+                  <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='existencia'>
+                    Existencia
+                    </label>
+                    <input
+                      className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      id='existencia'
+                      type='number'
+                      placeholder='Existencia Producto'
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.existencia}
+                      />
+                  </div>
 
-              {/*formik.touched.existencia && formik.errors.existencia ? (
-                <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4'>
-                  <p className='font-bold'>Error</p>
-                  <p>{formik.errors.existencia}</p>
-                </div>
-              ) : null*/}
+                  {props.touched.existencia && props.errors.existencia ? (
+                    <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4'>
+                    <p className='font-bold'>Error</p>
+                    <p>{props.errors.existencia}</p>
+                    </div>
+                  ) : null}
 
-              <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='precio'>
-                  Precio
-                </label>
-                <input
-                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                  id='precio'
-                  type='number'
-                  placeholder='Precio Producto'
-                  //onChange={formik.handleChange}
-                  //onBlur={formik.handleBlur}
-                  //value={formik.values.precio}
-                />
-              </div>
+                  <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='precio'>
+                    Precio
+                    </label>
+                    <input
+                      className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                      id='precio'
+                      type='number'
+                      placeholder='Precio Producto'
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      value={props.values.precio}
+                      />
+                  </div>
 
-              {/*formik.touched.precio && formik.errors.precio ? (
-                <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4'>
-                  <p className='font-bold'>Error</p>
-                  <p>{formik.errors.precio}</p>
-                </div>
-              ) : null*/}
+                  {props.touched.precio && props.errors.precio ? (
+                    <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4'>
+                    <p className='font-bold'>Error</p>
+                    <p>{props.errors.precio}</p>
+                    </div>
+                  ) : null}
 
-              <input
-              type='submit'
-              className='bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-600'
-              value='Registrar producto'
-              />
+                  <input
+                    type='submit'
+                    className='bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-600'
+                    value='Registrar producto'
+                  />
 
-            </form>
+                </form>
+              )
+            }}
           </Formik>
         </div>
       </div>
